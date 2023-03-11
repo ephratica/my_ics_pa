107a6b574c9520cdd92fa607694a41662972d72a
tracer-ics2017 <tracer@njuics.org>
2023-03-11 15:39:47 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:39:47 up  7:37,  1 user,  load average: 1.92, 1.64, 1.24 287a2c3f9916c7586c2529d99542fc6ec4df5a4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -51,14 +51,14 @@ static int cmd_info(char *args) {
     printf("error! info r: print registers. info w: print watch point\n");
   }
   else if(strcmp("r", args) == 0){
-      printf("eax:\t%x\t%d\n", cpu.eax, cpu.eax);
-      printf("edx:\t%x\t%d\n", cpu.edx, cpu.edx);
-      printf("ecx:\t%x\t%d\n", cpu.ecx, cpu.ecx);
-      printf("ebx:\t%x\t%d\n", cpu.ebx, cpu.ebx);
-      printf("ebp:\t%x\t%d\n", cpu.ebp, cpu.ebp);
-      printf("esi:\t%x\t%d\n", cpu.esi, cpu.esi);
-      printf("esp:\t%x\t%d\n", cpu.esp, cpu.esp);
-      printf("eip:\t%x\t%d\n", cpu.eip, cpu.eip);
+      printf("eax:\t0x%04x\t%04d\n", cpu.eax, cpu.eax);
+      printf("edx:\t0x%04x\t%04d\n", cpu.edx, cpu.edx);
+      printf("ecx:\t0x%04x\t%04d\n", cpu.ecx, cpu.ecx);
+      printf("ebx:\t0x%04x\t%04d\n", cpu.ebx, cpu.ebx);
+      printf("ebp:\t0x%04x\t%04d\n", cpu.ebp, cpu.ebp);
+      printf("esi:\t0x%04x\t%04d\n", cpu.esi, cpu.esi);
+      printf("esp:\t0x%04x\t%04d\n", cpu.esp, cpu.esp);
+      printf("eip:\t0x%04x\t%04d\n", cpu.eip, cpu.eip);
   }
   else if(strcmp("w", args) == 0){
 
