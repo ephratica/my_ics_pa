f150dd9481ccf4216bfa0459fd5b721f77aa6567
2012885-Jingyi Chen <ephratica@foxmail.com>
2023-03-13 10:40:57 +0800

    a simple version

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -89,7 +89,7 @@ static int cmd_x(char *args) {
   // 0x only
   unsigned int expr = 0;
   sscanf(EXPR, "%x", &expr);
-  printf("expr:%d\n", expr);
+  //printf("expr:%d\n", expr);
   for(int i=0;i<n;i++){
     printf("addr:\t0x%08x\tvalue:\t0x%08x\n", (expr + i*4), vaddr_read(expr + i*4, 4));
   }
