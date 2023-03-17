11ab13872547e6b44b3b2ae725812ddcb471c9c0
tracer-ics2017 <tracer@njuics.org>
2023-03-17 11:59:21 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:59:21 up 25 min,  1 user,  load average: 1.03, 0.86, 0.59 204e037dc9104adf4be08741fa116b9349bb3b30

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -5,6 +5,7 @@
 
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
+static WP list[NR_WP];
 
 void init_wp_pool() {
   int i;
@@ -38,4 +39,37 @@ void free_wp(WP *wp){
   head = wp->next;
   wp->next = free_;
   free_ = wp;
+}
+
+void Print_wp(){
+  if (head == NULL) {
+    printf("no watchpoint\n");
+    return ;
+  }
+
+  WP *wp = head;
+  while (wp) {
+    printf("%d\t%s\t0x%08x\n", wp->NO, wp->expr, wp->value);
+    wp = wp->next;
+  }
+
+  return;
+}
+
+void change_list() {
+  WP *wp = head;
+  bool flag = true;
+  int cnt = 0;
+
+  while (wp) {
+    uint32_t value;
+    value = expr(wp->expr, &flag);
+    if (value != wp->value) {
+      wp->value = value;
+      list[cnt++] = *wp;
+    }
+      wp = wp->next;
+  }
+  list[cnt].NO = -1;
+  return ;
 }
