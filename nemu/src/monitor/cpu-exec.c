4ff33a0fcb49a51ec5c6e7825b796e3dcf33a93d
tracer-ics2017 <tracer@njuics.org>
2023-03-17 12:22:20 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:22:20 up 48 min,  1 user,  load average: 1.83, 1.51, 1.26 b4af915950294b87d2f92bb2c262d91eb9b2b7d

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "monitor/monitor.h"
+#include "monitor/watchpoint.h"
 
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
