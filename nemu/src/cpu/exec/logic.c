9937ee4ceaba984f68fada4ed586ffe058dea888
tracer-ics2017 <tracer@njuics.org>
2023-04-16 23:39:51 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:39:51 up  1:35,  1 user,  load average: 1.11, 0.67, 0.55 5754526bc4369066dda0d96ec0d2d23e5428a2be

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -14,7 +14,7 @@ make_EHelper(and) {
   // TODO();
 	rtl_and(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
-  printf("result:0x%x\n", t0);
+  // printf("result:0x%x\n", t0);
 
 	rtl_update_ZFSF(&t0, id_dest->width);
 
