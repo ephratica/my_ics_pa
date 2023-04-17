98c14f52a9156a39adcc4b0793b67e7e53e97cec
tracer-ics2017 <tracer@njuics.org>
2023-04-17 11:35:38 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:35:38 up  3:12,  1 user,  load average: 1.45, 0.96, 0.75 3bce284db0215525eeccf673bc67d9f0523f16f8

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -195,11 +195,6 @@ void difftest_step(uint32_t eip) {
     printf("eip = 0x%x:\tcpu.eip = 0x%x\tr.eip = 0x%x)",
            eip, cpu.eip, r.eip);
   }
-    if (r.eflags != cpu.eflags){
-    diff = true;
-    printf("eip = 0x%x:\tcpu.eflags = 0x%x\tr.eflags = 0x%x)",
-           eip, cpu.eflags, r.eflags);
-  }
 
   if (diff) {
     nemu_state = NEMU_END;
