da5c311b6b426cd50f5f5e4ba9f954a636427dcd
tracer-ics2017 <tracer@njuics.org>
2023-04-17 10:58:20 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:58:20 up  2:35,  1 user,  load average: 1.21, 1.13, 1.13 3d11d7e762bee0b4c394a971e17dd3cb2370d348

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -41,7 +41,7 @@ static inline make_DopHelper(SI) {
   // TODO();
   if(op->width == 4)op->simm = (int32_t)instr_fetch(eip, op->width);
   if(op->width == 1)op->simm = (int8_t)((uint8_t)instr_fetch(eip, op->width));
-  rtl_li(&op->val, op->simm);
+  if(load_val)rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
