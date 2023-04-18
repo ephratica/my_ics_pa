48e04520d0f31c1230654a7fdf577cd129e12850
tracer-ics2017 <tracer@njuics.org>
2023-04-18 13:51:41 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:51:41 up  9:55,  1 user,  load average: 0.76, 0.75, 0.63 7406ae4785d9236619d733738fbb9bd4dc8ea3d

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -171,7 +171,8 @@ static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+  // TODO();
+  *dest = (*src1 != 0 ? 1 : 0);
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
