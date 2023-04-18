edb87f54c1763adcc272bc214233c587abc43a8f
tracer-ics2017 <tracer@njuics.org>
2023-04-18 13:25:53 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:25:53 up  9:29,  1 user,  load average: 1.16, 0.91, 0.59 5a040f72ce514009215af2204240945970c84cc5

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,7 +23,7 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   for(int i=0; i<h && y+i<_screen.height; i++){
-    memcpy(fb + ((y+i)*_screen.width + x), pixels, w*4);
+    memcpy(fb + ((y+i)*_screen.width + x), pixels + w*i, w*4);
   }
 }
 
