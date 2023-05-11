92bce279c163d41a7807640fde174670d443ed3d
tracer-ics2017 <tracer@njuics.org>
2023-05-11 20:04:28 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:04:28 up  8:56,  1 user,  load average: 0.71, 0.51, 0.43 6434cae1183fb04256614fb5f66ffa7d2dcc70ef

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,11 +4,17 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[3] = SYSCALL_ARG3(r);
+  a[4] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
     case SYS_none:
 			SYSCALL_ARG1(r) = 1;
 			break;
+    case SYS_exit:
+      _halt(a[1]);
+      break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
