3a16cc6e8dd3c296736c742380003948fbf70314
tracer-ics2017 <tracer@njuics.org>
2023-05-11 13:16:40 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:16:40 up  2:08,  1 user,  load average: 1.54, 0.92, 0.55 695fdae5baf2ddf9fedb5ce676fa825b1ad9717c

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -61,3 +61,4 @@ make_EHelper(nop);
 make_EHelper(in);
 make_EHelper(out);
 make_EHelper(lidt);
+make_EHelper(int);
--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -3,6 +3,8 @@
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
+extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
+
 make_EHelper(lidt) {
   // TODO();
   assert(!decoding.is_operand_size_16);
@@ -29,7 +31,8 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  TODO();
+  // TODO();
+  raise_intr(id_dest->val, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
 
