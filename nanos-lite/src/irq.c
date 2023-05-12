8daf45593f96f6fb2f7e70396f743e62a227dbb9
tracer-ics2017 <tracer@njuics.org>
2023-05-12 21:24:31 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:24:31 up 10:25,  1 user,  load average: 0.64, 0.52, 0.61 10c652f3b46cfd53c1d565c9f7f935379f5b7589

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
+    Log("event\n");
 			return do_syscall(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
