5ac642820f947c647df10198fee102d88515ab8d
tracer-ics2017 <tracer@njuics.org>
2023-05-13 14:27:05 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  14:27:05 up 11:18,  1 user,  load average: 1.23, 1.04, 0.87 f452444e628679f231b67330a925b303639e06d0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -37,7 +37,7 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_brk:
     Log("brk\n");
-      _heap.end = (void *)a[1];
+      // _heap.end = (void *)a[1];
       SYSCALL_ARG1(r) = 0;
       break;
     case SYS_open:
