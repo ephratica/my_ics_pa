9fc2042af9ca018f54976717383d66850001f3de
tracer-ics2017 <tracer@njuics.org>
2023-05-14 22:35:42 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:35:42 up 13:46,  1 user,  load average: 1.39, 0.95, 0.83 424e4e75a9f9c1dc9f999bc136e94eab1f2b88e4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,10 +3,18 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  // ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  int fd = fs_open(filename, 0, 0);
+	Log("fd: %d\n", fd);
+	fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); 
+	fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
