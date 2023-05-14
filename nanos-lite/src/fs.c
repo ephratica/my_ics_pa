fc06ef05eaa2328efcb1b8e51dfd53ad82eae466
tracer-ics2017 <tracer@njuics.org>
2023-05-14 22:46:42 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:46:42 up 13:57,  1 user,  load average: 0.70, 0.79, 0.80 cea59579a9d01a23e9f7f548970995c772373b28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -54,6 +54,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
       if(file_table[fd].open_offset + len > f_size)
         return 0;
       len = len < f_size - file_table[fd].open_offset ? len: f_size - file_table[fd].open_offset;
+      Log("ramdisk read\n");
 			ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 			file_table[fd].open_offset += len;
       assert(file_table[fd].open_offset <= f_size);
