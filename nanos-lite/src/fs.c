653d26d4657e8de6f320067009ff94b8dd56f4d3
tracer-ics2017 <tracer@njuics.org>
2023-05-14 22:26:07 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:26:07 up 13:36,  1 user,  load average: 1.56, 1.21, 0.86 9c6fd3f99cc770b6cade90dbb2d9beb89fc2d8c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,13 @@
 #include "fs.h"
 
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+
 typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -23,4 +27,82 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  file_table[FD_FB].size = _screen.width * _screen.height * 4;
+}
+
+int fs_open(const char *pathname, int flags, int mode){
+  for(int i = 0; i < NR_FILES; i++){
+    if(strcmp(pathname, file_table[i].name) == 0)return i;
+  }
+  Log("no such file error\n");
+  assert(0);
+  return 0;
+}
+
+size_t fs_filesz(int fd){
+  return file_table[fd].size;
+}
+
+
+ssize_t fs_read(int fd, void *buf, size_t len) {
+	ssize_t f_size = fs_filesz(fd);
+	switch(fd) {
+		case FD_STDOUT:
+		case FD_FB:
+			break;
+		default:
+      if(file_table[fd].open_offset + len > f_size)
+        return 0;
+      len = len < f_size - file_table[fd].open_offset ? len: f_size - file_table[fd].open_offset;
+			ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+      assert(file_table[fd].open_offset <= f_size);
+			break;
+	}
+	return len;
+}
+
+ssize_t fs_write(int fd, const void *buf, size_t len) {
+	ssize_t f_size = fs_filesz(fd);
+	switch(fd) {
+		case FD_STDOUT:
+		case FD_STDERR:
+			for(int i = 0; i < len; i++) {
+					_putc(((char*)buf)[i]);
+			}
+			break;
+		default:
+      if(file_table[fd].open_offset + len > f_size)
+        return 0;
+			len = len < f_size - file_table[fd].open_offset ? len: f_size - file_table[fd].open_offset;
+			ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+			file_table[fd].open_offset += len;
+      assert(file_table[fd].open_offset <= f_size);
+			break;
+	}
+	return len;
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence){
+  switch (whence)
+  {
+  case SEEK_SET:
+    file_table[fd].open_offset = offset;
+    break;
+  case SEEK_CUR:
+    file_table[fd].open_offset += offset;
+    break;
+  case SEEK_END:
+    file_table[fd].open_offset = file_table[fd].size + offset;
+    break;
+  
+  default:
+    return -1;
+    break;
+  }
+  return file_table[fd].open_offset;
+}
+
+int fs_close(int fd){
+  return 0;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,12 @@
 #include "common.h"
 #include "syscall.h"
 
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern off_t fs_lseek(int fd, off_t offset, int whence);
+extern int fs_close(int fd);
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -21,20 +27,7 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_write:
     // Log("write. len:%d\n", (size_t)a[3]);
-      switch ((int)a[1]){
-        case 1:
-        case 2:
-          for(int i = 0; i < (size_t)a[3]; i++) {
-            _putc(((char*)a[2])[i]);
-            // Log("%c", ((char*)a[2])[i]);
-            // Log("\n");
-          }
-          SYSCALL_ARG1(r) = (size_t)a[3];
-          break;
-        default:
-          // Log("a[1]: %d\n", (int)a[1]);
-          break;
-      }
+      SYSCALL_ARG1(r) = fs_write(a[1], (void *)a[2], a[3]);
       break;
     case SYS_brk:
     // Log("brk\n");
@@ -43,12 +36,20 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_open:
     // Log("1\n");
-			SYSCALL_ARG1(r) = 1;
+			SYSCALL_ARG1(r) = fs_open((char *)a[1], a[2], a[3]);
 			break;
     case SYS_read:
     // Log("2\n");
-			SYSCALL_ARG1(r) = 1;
+			SYSCALL_ARG1(r) = fs_read(a[1], (void *)a[2], a[3]);
 			break;
+    case SYS_lseek:
+      Log("sleek\n");
+      SYSCALL_ARG1(r) = fs_lseek(a[1], a[2], a[3]);
+      break;
+    case SYS_close:
+      Log("close\n");
+      SYSCALL_ARG1(r) = fs_close(a[1]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
