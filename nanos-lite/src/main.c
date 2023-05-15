283574a1d3e4e9ed270332c8275febe9b1a073fe
tracer-ics2017 <tracer@njuics.org>
2023-05-15 17:12:44 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:12:44 up 17:06,  1 user,  load average: 1.02, 0.49, 0.48 a626b91c1dccf0b9457e026d38f92becba32e2b0

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -32,7 +32,9 @@ int main() {
 
   // uint32_t entry = loader(NULL, "/bin/text");
   // uint32_t entry = loader(NULL, "/bin/bmptest");
-  uint32_t entry = loader(NULL, "/bin/events");
+  // uint32_t entry = loader(NULL, "/bin/events");
+  uint32_t entry = loader(NULL, "/bin/pal");
+
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
