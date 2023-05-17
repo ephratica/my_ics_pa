19e27e2a89f36817cb034bb431199cab44c9cb9c
tracer-ics2017 <tracer@njuics.org>
2023-05-17 18:26:38 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:26:38 up 1 day,  3:42,  1 user,  load average: 0.96, 0.51, 0.34 65a7c632fac849f484bc6f0f5e7035b2115a927c

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,10 +30,10 @@ int main() {
 
   init_fs();
 
-  // uint32_t entry = loader(NULL, "/bin/text");
+  uint32_t entry = loader(NULL, "/bin/text");
   // uint32_t entry = loader(NULL, "/bin/bmptest");
   // uint32_t entry = loader(NULL, "/bin/events");
-  uint32_t entry = loader(NULL, "/bin/pal");
+  // uint32_t entry = loader(NULL, "/bin/pal");
 
   ((void (*)(void))entry)();
 
