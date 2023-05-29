bae9d3413132a1c32ecde005be38b6cac12fb07b
tracer-ics2017 <tracer@njuics.org>
2023-05-29 22:59:35 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:59:35 up  3:32,  1 user,  load average: 0.95, 0.70, 0.58 c983a836b4296cbc4db26a5854dba158dc9d0212

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,22 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
+uint32_t get_pte_addr(uint32_t pte){
+  return (pte & 0xfffff000);
+}
+
+uint32_t get_pdx(uint32_t va){
+  return ((va >> 22) & 0x3ff);
+}
+
+uint32_t get_ptx(uint32_t va){
+  return ((va >> 12) & 0x3ff);
+}
+
+uint32_t get_off(uint32_t va){
+  return (va & 0xfff);
+}
+
 uint32_t paddr_read(paddr_t addr, int len) {
   int num = is_mmio(addr);
   if(num == -1)
@@ -28,10 +44,50 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
     mmio_write(addr, len, data, num);
 }
 
+uint32_t page_translate(vaddr_t addr, bool iswrite){
+  if(cpu.PG == 1){
+    uint32_t* base1 = (uint32_t*)get_pte_addr(cpu.cr3);
+    uint32_t pde = (uint32_t)paddr_read((uint32_t)(get_pdx(addr) + base1), 4);
+    assert(pde & 0x1);
+
+    uint32_t* base2 = (uint32_t*)get_pte_addr(pde);
+    uint32_t pte = (uint32_t)paddr_read((uint32_t)(get_pdx(addr) + base2), 4);
+    assert(pte & 0x1);
+
+    pde |= 0x20;
+    pte |= 0x20;
+
+    if (iswrite) {
+			pte |= 0x40;
+		}
+    paddr_write((uint32_t)(get_pdx(addr) + base1), 4, pde);
+		paddr_write((uint32_t)(get_pdx(addr) + base2), 4, pte);
+
+    return get_pte_addr(pte) | get_off(addr);
+  }
+  else{
+    return addr;
+  }
+}
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(get_pte_addr(addr) != get_pte_addr(addr + len -1)){
+    Log("in different pages\n");
+    assert(0);
+  }
+  else{
+    paddr_t paddr = page_translate(addr, false);
+		return paddr_read(paddr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if(get_pte_addr(addr) != get_pte_addr(addr + len -1)){
+    Log("in different pages\n");
+    assert(0);
+  }
+  else{
+    paddr_t paddr = page_translate(addr, true);
+		paddr_write(paddr, len, data);
+  }
 }
