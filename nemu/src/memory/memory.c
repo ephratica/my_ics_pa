abd3f603677c7d637894824be6be00c8bab62a97
tracer-ics2017 <tracer@njuics.org>
2023-05-29 23:23:47 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:23:47 up  3:56,  1 user,  load average: 1.26, 1.02, 0.80 5ed2ec222a145ce9b5629f66b7ffc966d664e856

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -17,11 +17,11 @@ uint32_t get_pte_addr(uint32_t pte){
 }
 
 uint32_t get_pdx(uint32_t va){
-  return ((va >> 22) & 0x3ff);
+  return ((va >> 22) & 0x3ff) << 2;
 }
 
 uint32_t get_ptx(uint32_t va){
-  return ((va >> 12) & 0x3ff);
+  return ((va >> 12) & 0x3ff) << 2;
 }
 
 uint32_t get_off(uint32_t va){
@@ -46,7 +46,7 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 
 uint32_t page_translate(vaddr_t addr, bool iswrite){
   if(cpu.PG == 1){
-    uint32_t* base1 = (uint32_t*)get_pte_addr(cpu.cr3);
+    uint32_t* base1 = (uint32_t*)cpu.cr3;
     uint32_t pde = (uint32_t)paddr_read((uint32_t)(get_pdx(addr) + base1), 4);
     if (!(pde & 0x1)) {
 			Log("addr = 0x%x, iswrite = %d", addr, iswrite);
