106bcbfa7071bae26dd81b90432eddd248ff92d4
tracer-ics2017 <tracer@njuics.org>
2023-05-29 21:52:22 +0800

    >  compile 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:52:22 up  2:25,  1 user,  load average: 1.53, 1.18, 0.77 97dbf6ef726266e9fe5500f98f1301c02c0ba7cf

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -17,14 +17,35 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
+  // TODO();
+  switch (id_dest->reg){
+    case 0:
+      cpu.cr0 = id_src->val;
+      break;
+    case 3:
+      cpu.cr3 = id_src->val;
+      break;
+    
+    default:
+      break;
+  }
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
-
+  // TODO();
+  switch (id_dest->reg){
+    case 0:
+      id_src->val = cpu.cr0;
+      break;
+    case 3:
+      id_src->val = cpu.cr3;
+      break;
+    
+    default:
+      break;
+  }
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
