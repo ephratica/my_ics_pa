e15a67e1c8ca0b2e874ac1f3a0391447313e966c
tracer-ics2017 <tracer@njuics.org>
2023-05-30 21:01:54 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:01:54 up 13:41,  1 user,  load average: 2.13, 1.40, 1.05 c217060c9110cd7bfe3bc409a00388625b696b2f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -16,7 +16,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       return schedule(r);
       break;
     case _EVENT_IRQ_TIME: 
-      Log("asye pass\n");
+      // Log("asye pass\n");
       return schedule(r);
       break;
     default: panic("Unhandled event ID = %d", e.event);
