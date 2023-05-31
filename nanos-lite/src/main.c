87768b6e7123976d9c3704eb7055473b1832a484
tracer-ics2017 <tracer@njuics.org>
2023-05-31 15:59:15 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:59:15 up 15:03,  1 user,  load average: 1.52, 1.05, 0.83 e5f90f5bfa174f62449d304c3a6b5628b658bcf8

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -39,9 +39,9 @@ int main() {
 
   // ((void (*)(void))entry)();
 
-  // load_prog("/bin/dummy");
-  load_prog("/bin/pal");
-  load_prog("/bin/hello");
+  load_prog("/bin/dummy");
+  // load_prog("/bin/pal");
+  // load_prog("/bin/hello");
 
   _trap();
 
