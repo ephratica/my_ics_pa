34fab6162f90ff6b276f3100471345d6d50f3c6f
tracer-ics2017 <tracer@njuics.org>
2023-06-01 13:18:15 +0800

    >  run 2012885 ephratica Linux ephratica-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:18:15 up 17:07,  1 user,  load average: 1.62, 1.20, 0.78 531fafc772dc1d81400718457fe92e82212ae6df

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -38,8 +38,11 @@ _RegSet* schedule(_RegSet *prev) {
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
   static int count = 0;
-  if(current == &pcb[1] || current == &pcb[0])count ++;
-  else current = &pcb[current_game];
+  // if(current == &pcb[1] || current == &pcb[0])count ++;
+  // else current = &pcb[current_game];
+
+  if(current == &pcb[2])current = &pcb[current_game];
+  else count ++;
 
   if(count == 1000){
     current = &pcb[2];
